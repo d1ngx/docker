d6320c7b23ee0830bd24f4da4f1d9690570993ff
root <root@localhost.localdomain>
2020-08-03 10:27:40 +0800

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -eu
+
+# return true if specified directory is empty
+directory_empty() {
+    [ -z "$(ls -A "$1/")" ]
+}
+
+# if [ ! -f "/var/www/html/config/data/install.lock" ] && [ ! -f "/var/www/html/config/config/setting_user.php" ] 
+if  directory_empty "/var/www/html"; then
+        if [ "$(id -u)" = 0 ]; then
+            rsync_options="-rlDog --chown nginx:root"
+        else
+            rsync_options="-rlD"
+        fi
+        rsync $rsync_options --delete /usr/src/kodbox/ /var/www/html/
+else
+        echo "KODBOX has been configured!"
+fi
+
+exec "$@"
